22b015d7b113d4c132935a7c7df268a7958b0f05
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2022-03-22 19:09:41 -0400

    ajout accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcueilComponent } from './acueil/acueil.component';
 import { CreateEnseignantComponent } from './Enseignant/create-enseignant/create-enseignant.component';
 import { EnseignantDeatailsComponent } from './Enseignant/enseignant-deatails/enseignant-deatails.component';
 import { EnseignantListComponent } from './Enseignant/enseignant-list/enseignant-list.component';
@@ -7,11 +8,12 @@ import { SearchEnseignantComponent } from './Enseignant/search-enseignant/search
 import { UpdateEnseignantComponent } from './Enseignant/update-enseignant/update-enseignant.component';
 
 const routes: Routes = [
+  { path:'home',component:AcueilComponent},
   { path:'enseignants',component:EnseignantListComponent},
  { path:'create-enseignant',component:CreateEnseignantComponent},
  { path:'search-enseignant',component:SearchEnseignantComponent},
- 
- { path:'',redirectTo :'enseignants',pathMatch:'full'},
+
+ { path:'',redirectTo :'home',pathMatch:'full'},
  { path:'update-enseignant/:id',component:UpdateEnseignantComponent},
  { path:'enseignant-details/:id',component:EnseignantDeatailsComponent}
 
