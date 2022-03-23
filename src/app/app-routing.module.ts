b4e54411e8f916161b28a8fdb43decca85428c44
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2022-03-23 01:09:18 -0400

    ajout type cours

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcueilComponent } from './acueil/acueil.component';
+import { CreateTypeCoursComponent } from './Cours/create-type-cours/create-type-cours.component';
+import { TypeCoursDetailsComponent } from './Cours/type-cours-details/type-cours-details.component';
+import { TypeCoursListComponent } from './Cours/type-cours-list/type-cours-list.component';
+import { UpdateTypeCoursComponent } from './Cours/update-type-cours/update-type-cours.component';
 import { CreateEnseignantComponent } from './Enseignant/create-enseignant/create-enseignant.component';
 import { EnseignantDeatailsComponent } from './Enseignant/enseignant-deatails/enseignant-deatails.component';
 import { EnseignantListComponent } from './Enseignant/enseignant-list/enseignant-list.component';
@@ -12,8 +16,8 @@ import { FormationListComponent } from './Formation/formation-list/formation-lis
 import { UpdateFormationComponent } from './Formation/update-formation/update-formation.component';
 
 const routes: Routes = [
-  { path:'home',component:AcueilComponent},
-  { path:'enseignants',component:EnseignantListComponent},
+ { path:'home',component:AcueilComponent},
+ { path:'enseignants',component:EnseignantListComponent},
  { path:'create-enseignant',component:CreateEnseignantComponent},
  { path:'search-enseignant',component:SearchEnseignantComponent},
 
@@ -24,7 +28,12 @@ const routes: Routes = [
  { path:'formations',component:FormationListComponent},
  { path:'create-formation',component:CreateFormationComponent},
  { path:'update-formation/:id',component:UpdateFormationComponent},
- { path:'formation-details/:id',component:FormationDetailsComponent}
+ { path:'formation-details/:id',component:FormationDetailsComponent},
+
+ { path:'type-cours',component:TypeCoursListComponent},
+ { path:'create-type-cours',component:CreateTypeCoursComponent},
+ { path:'update-type-cours/:id',component:UpdateTypeCoursComponent},
+ { path:'type-cours-details/:id',component:TypeCoursDetailsComponent}
 ];
 
 @NgModule({
